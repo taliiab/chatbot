e6863c2f378e321945e501df6bf544210c78def9
taliiab <taliabosii@gmail.com>
2026-06-17 02:59:28 -0300

    add usuario

---
+++ back/back/src/main/java/com/app/back/models/Usuario.java
@@ -0,0 +1,25 @@
+package com.app.back.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
